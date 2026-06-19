39a79a6a35cee732f7564de88a8f248ffc83986d
samddsdev <samddsdev@gmail.com>
2026-06-19 21:36:10 +0100

    Added Program Interrupt loop

--- coffeeShopRobotv2.py
+++ coffeeShopRobotv2.py
@@ -22,6 +22,7 @@ def loadingdots() :
 
 #Find current local time
 import time
+import sys
 local_time = time.localtime()
 #print(local_time.tm_hour)
 
@@ -33,45 +34,56 @@ elif local_time.tm_hour >= 12:
 else:
     salutation = "morning"
 
-loadingdots()
+try: # Listens for Ctrl+C to end program cleanly.
+    loadingdots()
 
-### Greeting Section: ###
-clear_screen()
-print(f"Good {salutation} and welcome to our coffee shop!\n")
-time.sleep(0.5) # breifly pause the program for (seconds)
-name = input("What is your name?\n")
+    ### Greeting Section: ###
+    clear_screen()
+    print(f"Good {salutation} and welcome to our coffee shop!\n")
+    time.sleep(0.5) # breifly pause the program for (seconds)
+    name = input("What is your name?\n")
 
-print("\nHi " + name + ", thank you for coming in today!\n")
+    print("\nHi " + name + ", thank you for coming in today!\n")
 
-### Taking order section: ###
-clear_screen()
+    ### Taking order section: ###
+    clear_screen()
 
-print("What would you like to order " + name + "?\n")
-time.sleep(0.5)
+    print("What would you like to order " + name + "?\n")
+    time.sleep(0.5)
 
-# Store coffee menu as list
-coffee_menu = ["Americano", "Cappuccino", "Caf\u00e9 Latte", "Espresso", "Flat White"]
+    # Store coffee menu as list
+    coffee_menu = ["Americano", "Cappuccino", "Caf\u00e9 Latte", "Espresso", "Flat White"]
 
-# Displays the coffee menu. Uses a for loop to count and print the coffee_menu list
-for number, drink in enumerate(coffee_menu, start=1):
-    print(f"{number}. {drink}")
-    time.sleep(0.05)
+    # Displays the coffee menu. Uses a for loop to count and print the coffee_menu list
+    for number, drink in enumerate(coffee_menu, start=1):
+        print(f"{number}. {drink}")
+        time.sleep(0.05)
 
 
-# Make sure the user enters an integer
-while True:
-    try: 
-        coffee_selection = int(input("\nPlease enter the number option of your choice.\n"))
+    # Make sure the user enters an integer
+    while True:
+        try: 
+            coffee_selection = int(input("\nPlease enter the number option of your choice.\n"))
 
-        # Make sure number entered is between 1 and 5. 
-        if 1 <= coffee_selection <= len(coffee_menu): # If 1 is less than or equal to the number entered and less than or equal to the length of the coffee list.
-            break # Valid input, continue with program.
-        else:
-            print(f"\033[31mInvalid input:\033[0m Please type a number between 1 and  {len(coffee_menu)}.\n")
-    
-    except ValueError: 
-        print("\033[31mInvalid input:\033[0m Please type a whole number.\n")
+            # Make sure number entered is between 1 and 5. 
+            if 1 <= coffee_selection <= len(coffee_menu): # If 1 is less than or equal to the number entered and less than or equal to the length of the coffee list.
+                break # Valid input, continue with program.
+            else:
+                print(f"\033[31mInvalid input:\033[0m Please type a number between 1 and  {len(coffee_menu)}.\n")
+        
+        except ValueError: 
+            print("\033[31mInvalid input:\033[0m Please type a whole number.\n")
 
-loadingdots()
+    loadingdots()
 
-print("\nYour " + coffee_menu[coffee_selection - 1] + " is coming right up!\n")
+    print("\nYour " + coffee_menu[coffee_selection - 1] + " is coming right up!\n")
+
+
+
+
+
+
+
+except KeyboardInterrupt: # When Ctrl+C is pressed, end the program cleanly.
+    print("\033[31m -- Program Interrupted --\033[0m")
+    sys.exit(0) # cleanly ends program without errors.
