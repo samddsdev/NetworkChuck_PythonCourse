442670d4d60e6e46ffbacea0c57878df29abacc9
samddsdev <samddsdev@gmail.com>
2026-06-22 14:08:20 +0100

    v2.1 complete (if statements practice & maths)

--- coffeeShopRobotv2.1.py
+++ coffeeShopRobotv2.1.py
@@ -1,10 +1,15 @@
-# Clear the terminal screen before running Python script.
-import os # imports the module that allows Python to talk to the OS terminal.
-# 'nt' means Windows, 'posix' means a Unix based OS
-# define it as a function to be called later in the program
-def clear_screen(): os.system('cls' if os.name == 'nt' else 'clear')
+############# Program a robot to take your coffee order ######################  (NetworkChuck Youtube Python course) 
 
-def loadingdots(text, duration_cycles) : # Fake loading animation. Can be customised with "text" and how many times to loop it
+#imports:
+import sys # Used to manipulate the Python runtime. Needed for sys.exit()
+import os # Allows Python to talk to the Operating System. Needed for clear_screen()
+import time # Needed to access system time. Needed for time.sleep()
+
+# Clear the terminal screen before running Python script.
+def clear_screen(): os.system('cls' if os.name == 'nt' else 'clear') # 'nt' means Windows, 'posix' means a Unix based OS
+
+# Fake loading animation. Can be customised with "text" and how many times to loop it
+def loadingdots(text, duration_cycles) : 
     loadCount = 0
     while loadCount < duration_cycles:
         clear_screen()
@@ -17,13 +22,8 @@ def loadingdots(text, duration_cycles) : # Fake loading animation. Can be custom
         time.sleep(0.3)
         loadCount += 1
 
-############# Program a robot to take your coffee order ######################
-
 #Find current local time
-import time # Needed for time
-import sys # Needed for the sleep function 
 local_time = time.localtime() # Presents local time in human readable format. Used to extract .tm_hour value.
-#print(local_time.tm_hour)
 
 # Change salutation based on time of day; morning, afternoon, evening.
 if local_time.tm_hour >= 18:
@@ -34,19 +34,25 @@ else:
     salutation = "morning"
 
 
-### Main program starts here: ###
+#### Main program starts here: ####
 
 try: # "Listens" for Ctrl+C input to end program cleanly.
 
     ## Greeting Section: ##
     clear_screen()
     print(f"Good {salutation} and welcome to our coffee shop!\n")
-    time.sleep(0.5) # breifly pause the program for (seconds)
+    time.sleep(0.5) # breifly pauses the program for (seconds)
     name = input("What is your name?\n")
+    name = name.capitalize() # changes customer name to sentence case (i.e. Sam not sam, SAM, sAm etc.)
 
-    print("\nHi " + name + ", thank you for coming in today!\n")
+    if name == "Ben": # denies Ben entry to coffee shop and ends program
+        clear_screen()
+        print("You're not welcome in here " + name + "! GET OUT!!")
+        sys.exit(0) # ends the program cleanly without errors (0)
+    else:
+        print("\nHi " + name + ", thank you for coming in today!\n")
 
-    ### Taking order section: ###
+    ## Taking order section: ##
     clear_screen()
 
     print("What would you like to order " + name + "?\n")
@@ -90,8 +96,10 @@ try: # "Listens" for Ctrl+C input to end program cleanly.
 
     if no_of_coffees > 1:
         print(f"\nThank you {name}, your {no_of_coffees} {coffee_menu[coffee_selection - 1]}s are coming right up!\n")
-    else: print(f"\nThank you {name}, your {coffee_menu[coffee_selection - 1]} is coming right up!\n")
+    else: 
+        print(f"\nThank you {name}, your {coffee_menu[coffee_selection - 1]} is coming right up!\n")
+    exit()
 
 except KeyboardInterrupt: # When Ctrl+C is pressed, end the program cleanly.
     print("\033[31m -- Program Interrupted --\033[0m")
-    sys.exit(0) # cleanly ends program without errors.
+    sys.exit(0) # ends the program cleanly without errors (0)
